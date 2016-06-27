56ca13293bae4e9659601cef9c2517ca4ba71aee
Rober Pastor <rober@square1.io>
2016-06-27 09:30:45 +0200

    Initial project

---
+++ SQ1Tools-ObjC/SQ1Tools-ObjC.h
@@ -0,0 +1,19 @@
+//
+//  SQ1Tools-ObjC.h
+//  SQ1Tools-ObjC
+//
+//  Created by Rober Pastor on 27/6/16.
+//  Copyright © 2016 Square1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQ1Tools-ObjC.
+FOUNDATION_EXPORT double SQ1Tools_ObjCVersionNumber;
+
+//! Project version string for SQ1Tools-ObjC.
+FOUNDATION_EXPORT const unsigned char SQ1Tools_ObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQ1Tools_ObjC/PublicHeader.h>
+
+
