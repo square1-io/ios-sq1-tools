12be9591affc5d4efb941f463a592ccb1971e45a
Rober Pastor <rober@square1.io>
2016-06-28 13:34:08 +0200

    Fix in inline functions

--- SQ1Tools-ObjC/Source/Helpers/SQ1Basics.h
+++ SQ1Tools-ObjC/Source/Helpers/SQ1Basics.h
@@ -8,19 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-#define user_defaults_get_bool(key)   [[NSUserDefaults standardUserDefaults] boolForKey:key]
-#define user_defaults_get_int(key)    ((int) [[NSUserDefaults standardUserDefaults] integerForKey:key])
-#define user_defaults_get_double(key) [[NSUserDefaults standardUserDefaults] doubleForKey:key]
-#define user_defaults_get_string(key) fc_safeString([[NSUserDefaults standardUserDefaults] stringForKey:key])
-#define user_defaults_get_array(key)  [[NSUserDefaults standardUserDefaults] arrayForKey:key]
-#define user_defaults_get_object(key) [[NSUserDefaults standardUserDefaults] objectForKey:key]
+#define userDefaultsGetBool(key)   [[NSUserDefaults standardUserDefaults] boolForKey:key]
+#define userDefaultsGetInt(key)    ((int) [[NSUserDefaults standardUserDefaults] integerForKey:key])
+#define userDefaultsGetDouble(key) [[NSUserDefaults standardUserDefaults] doubleForKey:key]
+#define userDefaultsGetString(key) fc_safeString([[NSUserDefaults standardUserDefaults] stringForKey:key])
+#define userDefaultsGetArray(key)  [[NSUserDefaults standardUserDefaults] arrayForKey:key]
+#define userDefaultsGetObject(key) [[NSUserDefaults standardUserDefaults] objectForKey:key]
 
-#define user_defaults_set_bool(key, b)   { [[NSUserDefaults standardUserDefaults] setBool:b    forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
-#define user_defaults_set_int(key, i)    { [[NSUserDefaults standardUserDefaults] setInteger:i forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
-#define user_defaults_set_double(key, d) { [[NSUserDefaults standardUserDefaults] setDouble:d  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
-#define user_defaults_set_string(key, s) { [[NSUserDefaults standardUserDefaults] setObject:s  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
-#define user_defaults_set_array(key, a)  { [[NSUserDefaults standardUserDefaults] setObject:a  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
-#define user_defaults_set_object(key, o) { [[NSUserDefaults standardUserDefaults] setObject:o  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetBool(key, b)   { [[NSUserDefaults standardUserDefaults] setBool:b    forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetInt(key, i)    { [[NSUserDefaults standardUserDefaults] setInteger:i forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetDouble(key, d) { [[NSUserDefaults standardUserDefaults] setDouble:d  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetString(key, s) { [[NSUserDefaults standardUserDefaults] setObject:s  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetArray(key, a)  { [[NSUserDefaults standardUserDefaults] setObject:a  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
+#define userDefaultsSetObject(key, o) { [[NSUserDefaults standardUserDefaults] setObject:o  forKey:key]; [[NSUserDefaults standardUserDefaults] synchronize]; }
 
 #define APP_DISPLAY_NAME    [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleDisplayName"]
 #define APP_VERSION         [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
